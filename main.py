9bc4904431d1bc8e6b92d736b0518d35ac42fb80
Roma Major <65121094+rmmajor@users.noreply.github.com>
2020-05-15 14:03:53 +0300

    Update main.py
    Empty keyboard added. 'start' and 'help' command changed ('help' now have own dicorator). Some preparations for the new mode added.

--- main.py
+++ main.py
@@ -23,12 +23,37 @@ def trans(text):
     return res
 
 
-@bot.message_handler(commands=['start', 'help'])
+select_mode_mrkp = tb.types.ReplyKeyboardMarkup(resize_keyboard=True)
+item1 = tb.types.KeyboardButton('Морзе -> текст')
+item2 = tb.types.KeyboardButton('текст -> Морзе')
+select_mode_mrkp.row(item1)
+select_mode_mrkp.row(item2)
+
+
+# @bot.message_handler(commands=['change_mode'])
+# def change_mode():
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.from_user.id,
+                     """Пожалуйста выберете режим работы бота:
+Режим [Морзе -> текст] Разшифроывает текст, написаный азбукой Морзе.
+Режим [текст -> Морзе] Зашифроывает текст, с помощью азбуки Морзе.
+Вы также можете использовать команду /change_mode для изменения режима работы
+""")
+
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message,
-                 """Этот бот преобразует ваш текст в шифр азбукой Морзе. 
-Для того, что бы зашифровать ваше сообщение, просто введите нужный текст.
-Символы, кодировка которых в Морзе не предусмотрена, будут просто выводится. """)
+                 """Этот бот - удобный инструмент для работы с азбукой Морзе.
+Существует два режима работы:
+Режим [Морзе -> текст] Разшифроывает текст, написаный азбукой Морзе.
+Режим [текст -> Морзе] Зашифроывает текст, с помощью азбуки Морзе.
+Используйте команду /help, чтобы узнать больше""")
+    # select_mode_mrkp = tb.types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(message.from_user.id, "test", reply_markup=select_mode_mrkp)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
